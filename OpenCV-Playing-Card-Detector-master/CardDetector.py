3345be13a08e5127570474025b2335f5c88d4471
Ali1215 <55634235+Ali1215@users.noreply.github.com>
2022-01-18 23:20:20 -0700

    Update CardDetector.py

--- OpenCV-Playing-Card-Detector-master/CardDetector.py
+++ OpenCV-Playing-Card-Detector-master/CardDetector.py
@@ -5,28 +5,10 @@ import numpy as np
 import time
 import os
 import Cards
-import VideoStream
+
 from card import card
 
 from skimage.metrics import structural_similarity as ssim
-
-### ---- INITIALIZATION ---- ###
-# Define constants and initialize variables
-
-## Camera settings
-# Initialize camera object and video feed from the camera. The video stream is set up
-# as a seperate thread that constantly grabs frames from the camera feed. 
-# See VideoStream.py for VideoStream class definition
-## IF USING USB CAMERA INSTEAD OF PICAMERA,
-## CHANGE THE THIRD ARGUMENT FROM 1 TO 2 IN THE FOLLOWING LINE:
-#videostream = VideoStream.VideoStream((IM_WIDTH,IM_HEIGHT),FRAME_RATE,1,0).start()
-#time.sleep(1) # Give the camera time to warm up
-
-# Load the train rank and suit images
-#path = os.path.dirname(os.path.abspath(__file__))
-#train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
-#train_suits = Cards.load_suits( path + '/Card_Imgs/')
-
 
 
 ### ---- MAIN LOOP ---- ###
@@ -35,33 +17,24 @@ from skimage.metrics import structural_similarity as ssim
 def identify_card():
 
     cam_quit = 0 # Loop control variable
-    #cam = cv2.VideoCapture('http://192.168.1.68:4747/video')
+    
     cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)   # 0 -> index of camera
     s, image = cam.read()
     image = cv2.rotate(image, cv2.cv2.ROTATE_90_CLOCKWISE)
     cv2.imwrite("capture_card.jpg",image)
 
-    #path1='./OpenCV-Playing-Card-Detector-master/WIN_20220116_02_43_47_Pro.jpg'
-
-    #image = cv2.imread(path1)
-
-    # Begin capturing frames
+    
 
 
-    # Grab frame from video stream
-
-
-
-    print('hi')
     # Pre-process camera image (gray, blur, and threshold it)
     pre_proc = Cards.preprocess_image(image)
 
     # Find and sort the contours of all cards in the image (query cards)
     cnts_sort, cnt_is_card = Cards.find_cards(pre_proc)
-    print('hi')
+
     # If there are no contours, do nothing
     if len(cnts_sort) != 0:
-        print('hi')
+        
         # Initialize a new "cards" list to assign the card objects.
         # k indexes the newly made array of cards.
         
