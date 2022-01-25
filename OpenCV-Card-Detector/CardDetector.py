b72f36d89cd35cf3b27eb292131bf74a6f0ff354
afaro4 <72225000+afaro4@users.noreply.github.com>
2022-01-24 23:50:32 -0700

    Update CardDetector.py

--- OpenCV-Card-Detector/CardDetector.py
+++ OpenCV-Card-Detector/CardDetector.py
@@ -10,10 +10,9 @@ from skimage.metrics import structural_similarity as ssim
 
 
 ### ---- MAIN LOOP ---- ###
-# The main loop repeatedly grabs frames from the video stream
-# and processes them to find and identify playing cards.
 def identify_card():
     
+    #sets up camera and takes photo of what it captured and saves it into capture_card.jpg
     cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)   # 0 -> index of camera
     s, image = cam.read()
     image = cv2.rotate(image, cv2.cv2.ROTATE_90_CLOCKWISE)
@@ -28,10 +27,7 @@ def identify_card():
     # If there are no contours, do nothing
     if len(cnts_sort) != 0:
         
-        # Initialize a new "cards" list to assign the card objects.
-        # k indexes the newly made array of cards.
-        
-        card = (Cards.preprocess_card(cnts_sort[0],image))
+        card = (Cards.preprocess_card(cnts_sort[0],image)) #flattens image
         cv2.imwrite("processed.jpg",card.warp)
         processedCard = card.warp
         maxScore = -2
@@ -41,7 +37,7 @@ def identify_card():
         for file in os.listdir("./OpenCV-Card-Detector/unoCardPictures"):
             #print(file)
             template = cv2.imread("./OpenCV-Card-Detector/unoCardPictures/"+file)
-            #calculate correlation coefficient using scikit-image
+            #calculate correlation coefficient using scikit-image, structural similarity index
             tempScore = ssim(processedCard, template,win_size=11,multichannel=True)
             
             if tempScore>maxScore:
